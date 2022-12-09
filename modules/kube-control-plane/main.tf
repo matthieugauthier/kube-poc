56336a76fc302ac9cb22af4b22aaa4f1b5422c2b
Matthieu GAUTHIER <matthieu.gauthier@grdf.fr>
2022-12-09 11:54:46 +0100

    Clean CP Install

--- modules/kube-control-plane/main.tf
+++ modules/kube-control-plane/main.tf
@@ -75,21 +75,27 @@ resource "aws_spot_instance_request" "control-plane" {
   sudo systemctl start rke2-server.service
 
   echo "*** Install HELM"
-  curl https://raw.githubusercontent.com/matthieugauthier/kube-poc/main/scripts/install-helm.sh | bash -
-  
-  echo "*** Make Local Kubectl Working"
+  curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | tee /usr/share/keyrings/helm.gpg
+  apt-get install apt-transport-https --yes
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | tee /etc/apt/sources.list.d/helm-stable-debian.list
+  apt-get update
+  apt-get install helm
+
+  echo "*** Configure Local Kubectl"
   sudo cp /etc/rancher/rke2/rke2.yaml /home/ubuntu/rke2.yaml
   sudo chown ubuntu:ubuntu /home/ubuntu/rke2.yaml
   ln -s /var/lib/rancher/rke2/bin/kubectl /usr/local/bin/kubectl
-  echo "export KUBECONFIG=/home/ubuntu/rke2.yaml" >> /home/ubuntu/.profile
-  export KUBECONFIG=/home/ubuntu/rke2.yaml
-  echo "Configure Cluster Name"
   CLUSTER_NAME=${replace(var.name, "kube-cp-","")}
-  echo $CLUSTER_NAME
   sed -i "s/default/$CLUSTER_NAME/" /home/ubuntu/rke2.yaml
   CLUSTER_IP=$(hostname -I|cut -d' ' -f1)
-  echo $CLUSTER_IP
   sed -i "s/127.0.0.1/$CLUSTER_IP/" /home/ubuntu/rke2.yaml
+  echo "export KUBECONFIG=/home/ubuntu/rke2.yaml" >> /home/ubuntu/.profile
+  export KUBECONFIG=/home/ubuntu/rke2.yaml
+
+  echo "*** Install Argocd CLI"
+  curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+  sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+  rm argocd-linux-amd64
 
   echo "*** Wait RKE2 OK"
   while [[ $(kubectl get po -A | grep -v Completed | grep -v Running | wc -l) != "1" ]]
@@ -97,13 +103,6 @@ resource "aws_spot_instance_request" "control-plane" {
       sleep 5
   done
   sleep 20
-
-  sed -i "s/###RANCHERIP###/${var.rancher_private_ip}/" /home/ubuntu/hostalias-rancherhost-patch.yml
-
-  curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
-    sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
-    rm argocd-linux-amd64
-
 
   if [ "${var.rancher_install_doit}" == "yes" ]; then
     echo "*** Install cert-manager"
@@ -123,7 +122,6 @@ resource "aws_spot_instance_request" "control-plane" {
 
     echo "*** Wait Rancher"
     kubectl -n cattle-system rollout status deploy/rancher
-    echo "*** Completed Installing RKE2 + Rancher"
  
     echo "*** Install argocd"
     sleep 60
@@ -134,34 +132,33 @@ resource "aws_spot_instance_request" "control-plane" {
     kubectl apply -n argocd -f /home/ubuntu/wild-tls.yml
     kubectl apply -n argocd -f /home/ubuntu/argocd.yml
     kubectl apply -n argocd -f /home/ubuntu/argocd-ui.yml
-    echo "*** Completed Installing argocd"
-
-    echo "Argocd Password:"
+    
+    echo "*** Prepare argocd password"
     sleep 30
     kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d > /home/ubuntu/token_argocd
 
   else
+    echo "*** Prepare etc hosts"
     echo "${var.rancher_private_ip} ${var.rancher_dns} ${var.argocd_dns}" >> /etc/hosts
-    echo "*** Completed Installing RKE2 Without Rancher"
-
+    
+    echo "*** Register cluster to argocd"
     sleep 230
-
-    echo "Get Remote Token Argo"
-    TOKEN=$(ssh -o StrictHostKeyChecking=no -i /home/ubuntu/.ssh/id_rsa ubuntu@${var.rancher_dns} cat /home/ubuntu/token_argocd)
-    echo $TOKEN
-    echo "Login Argo"
-    echo "argocd login ${var.argocd_dns} --username admin --password $TOKEN --grpc-web"
-    argocd login ${var.argocd_dns} --username admin --password $TOKEN --grpc-web
-    echo "Add current cluster to Argo"
+    ARGOCD_TOKEN=$(ssh -o StrictHostKeyChecking=no -i /home/ubuntu/.ssh/id_rsa ubuntu@${var.rancher_dns} cat /home/ubuntu/token_argocd)
+    argocd login ${var.argocd_dns} --username admin --password $ARGOCD_TOKEN --grpc-web
     argocd cluster add $CLUSTER_NAME --grpc-web --yes
 
-
-    echo "Run \"kubectl patch -n cattle-system deployment cattle-cluster-agent --patch-file /home/ubuntu/hostalias-rancherhost-patch.yml\" to patch after apply rancher conf"
+    echo "*** Prepare cluster rancher agent patch"
+    sed -i "s/###RANCHERIP###/${var.rancher_private_ip}/" /home/ubuntu/hostalias-rancherhost-patch.yml
   fi
+
 
   echo "************************************"
   echo ""
   echo "*** Completed Cloud Init"
+  echo ""
+  echo ""
+  echo "Cluster Name : $CLUSTER_NAME"
+  echo "Cluster IP   : $CLUSTER_IP"
   echo ""
   echo ""
   EOF
